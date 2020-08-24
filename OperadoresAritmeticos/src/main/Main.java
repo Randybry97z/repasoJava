a8c620fbcd9eae40568f7364a7dd2d58d6e36d9c
Bryan <bssandovalrod@gmail.com>
2020-08-24 12:23:16 -0500

    operadores aritméticos

---
+++ OperadoresAritmeticos/src/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Operadores aritméticos
+		
+		int variableX = 50, variableY = 10;
+		int resultado;
+		
+		resultado = variableX + variableY;
+		System.out.println(resultado);
+		
+		resultado = variableX - variableY;
+		System.out.println(resultado);
+		
+		resultado = variableX * variableY;
+		System.out.println(resultado);
+		
+		resultado = variableX / variableY;
+		System.out.println(resultado);
+		
+		
+		//Incrementos y decrementos
+		
+		variableX ++;
+		variableX --;
+		
+
+	}
+
+}
