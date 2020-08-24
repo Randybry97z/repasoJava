d2948b003b96129dc10483427aad3e26e84722dc
Bryan <bssandovalrod@gmail.com>
2020-08-24 13:13:47 -0500

    Métodos de cadenas pt2

--- Strings/src/main/Main.java
+++ Strings/src/main/Main.java
@@ -10,24 +10,40 @@ public class Main {
 
 		// MÉTODOS
 		
-		//Longitud
-		int cantidad = mensaje.length();
-		System.out.println(cantidad);
+//		//Longitud
+//		int cantidad = mensaje.length();
+//		System.out.println(cantidad);
+//		
+//		//Contains
+//		boolean res = mensaje.contains("Hola");
+//		System.out.println(res);
+//		
+//		//Start with || end with
+//		boolean inicia = mensaje.startsWith("Hola");
+//		
+//		boolean termina = mensaje.endsWith("v");
+//		
+//		System.out.println("Inicia: " + inicia + " ; " + "Termina: " + termina );
+//
+//		String cadena = mensaje.concat(" del curso de Java :v");
+//		System.out.println(cadena);
 		
-		//Contains
-		boolean res = mensaje.contains("Hola");
-		System.out.println(res);
+		//Uppercase && Lowercase
 		
-		//Start with || end with
-		boolean inicia = mensaje.startsWith("Hola");
+		System.out.println(mensaje.toUpperCase());
 		
-		boolean termina = mensaje.endsWith("v");
+		System.out.println(mensaje.toLowerCase());
 		
-		System.out.println("Inicia: " + inicia + " ; " + "Termina: " + termina );
-
-		String cadena = mensaje.concat(" del curso de Java :v");
-		System.out.println(cadena);
+		//Trim
+		System.out.println(mensaje.trim());
 		
+		//Format
+		String curso = "Java";
+		float valor = 10.82935f;
+		//mensaje = String.format("Bienvenido al curso %s", curso);
+		mensaje = String.format("El total de %d artículos es: %.2f %s", 3, valor, "MXN");
+		System.out.println(mensaje);
+		System.out.printf("El total de %d artículos es: %.2f %s", 3, valor, "MXN");
 	}
 
 }
