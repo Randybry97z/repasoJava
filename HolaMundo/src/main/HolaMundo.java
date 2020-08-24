a99de77229b378634d3226d93bb70a59a4cbf56e
Bryan <bssandovalrod@gmail.com>
2020-08-24 12:08:28 -0500

    Constantes

--- HolaMundo/src/main/HolaMundo.java
+++ HolaMundo/src/main/HolaMundo.java
@@ -4,11 +4,15 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo fuera del for :v");
-		for (int i = 0; i <= 5; i++) {
-			System.out.println("Hola Mundo"+i);
-		}
+//		System.out.println("Hola Mundo fuera del for :v");
+//		for (int i = 0; i <= 5; i++) {
+//			System.out.println("Hola Mundo"+i);
+//		}
 		
+		/* CONSTANTES */
+		final float pi = 3.1415f, e = 8.9f;
+		
+		System.out.println(pi + " " + e);
 	}
 
 }
