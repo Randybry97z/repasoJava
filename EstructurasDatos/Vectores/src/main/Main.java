44b5f4eba58520e9da3e5774168a9bde612b32e0
Bryan <bssandovalrod@gmail.com>
2020-09-02 13:16:30 -0500

    Vectores

--- EstructurasDatos/Vectores/src/main/Main.java
+++ EstructurasDatos/Vectores/src/main/Main.java
@@ -1,10 +1,21 @@
 package main;
 
+import java.util.Vector;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// VECTORES
+		
+		//Por defecto su longitud es de 10
+		Vector vector = new Vector(/*initialCapacity*/10,/*incrementCapacity*/5);
+		vector.add("he");
+		vector.add("Hola");
+		vector.insertElementAt(/*obj*/"jj", /*index*/1);
+		System.out.println(vector);
+		vector.remove("he");
+		System.out.println(vector);
+		System.out.println(vector.elementAt(0));
 	}
 
 }
